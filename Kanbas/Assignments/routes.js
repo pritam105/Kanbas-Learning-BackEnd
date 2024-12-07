d7e4320016f2971ba4630a881342dd5e8c38b620
pritam105 <pritammane105@mail.com>
2024-12-07 18:49:06 -0500

    add mongodb as backend

--- Kanbas/Assignments/routes.js
+++ Kanbas/Assignments/routes.js
@@ -1,16 +1,17 @@
 import * as assignmentDao from "./dao.js";
+
 export default function AssignmentRoutes(app) {
- app.delete("/api/assignments/:assignmentId", (req, res) => {
+ app.delete("/api/assignments/:assignmentId", async (req, res) => {
    const { assignmentId } = req.params;
-   assignmentDao.deleteAssignment(assignmentId);
-   res.sendStatus(204);
+   const status = await assignmentDao.deleteAssignment(assignmentId);
+   res.send(status);
  });
 
- app.put("/api/assignments/:assignmentId", (req, res) => {
+ app.put("/api/assignments/:assignmentId", async (req, res) => {
     const { assignmentId } = req.params;
     const assignmentUpdates = req.body;
-    assignmentDao.updateAssignment(assignmentId, assignmentUpdates);
-    res.sendStatus(204);
+    const status = await assignmentDao.updateAssignment(assignmentId, assignmentUpdates);
+    res.send(status);
   });
 
 }
